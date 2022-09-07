47891028d3fd2e670b93f9b7735c2e3d724b8712
Daniel Cooper <daniel.c321@stu.nebo.edu>
2022-09-07 14:19:05 -0600

    Created Balloon POP project

---
+++ Balloon POP/Assets/Scripts/Balloon.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int scoreToGive = 100; // score give for popped balloon.
+
+    public int clickToPop = 3; //How many clicks before balloon pops
+
+    public float scaleToIncrease = 0.10f; //scale increase each time the balloon is clicked
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        //reduce clicks by one
+        clickToPop -= 1;
+        //increase
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clickToPop == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
