2a9f672ea15bdb64b24cf58c6a529c69e2b7aa21
Daniel Cooper <daniel.c321@stu.nebo.edu>
2022-11-02 12:10:15 -0600

    workin with projectiles

--- RogueLike/Assets/Scripts/MoveAndShoot.cs
+++ RogueLike/Assets/Scripts/MoveAndShoot.cs
@@ -8,16 +8,13 @@ public class MoveAndShoot : MonoBehaviour
     public float stopDistance;
     public float retreatDistance;
     private  Transform target;
-    private float shotDelay;
     public float startDelay;
 
-    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
-        shotDelay = startDelay;
     }
 
     // Update is called once per frame
@@ -38,26 +35,7 @@ public class MoveAndShoot : MonoBehaviour
        {
            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);// Running Away from the player
        }       
-        //Ranged Attack Mechanic
-       if(shotDelay <= 0)
-       {
-          Attack(); 
-       }
        
-       else
-       {
-           shotDelay -= Time.deltaTime;
-       }
-
-    }
-    void Attack()
-    {
-       if(Vector2.Distance(transform.position, target.position) > stopDistance)
-       {           
-           transform.position = this.transform.position;//Stay put
-           Instantiate(projectile, transform.position, Quaternion.identity);//Fire Projectiles
-           shotDelay = startDelay; // Reset ShotDelay
-       } 
     }
 
         
