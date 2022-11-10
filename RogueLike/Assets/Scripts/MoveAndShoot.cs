a60409fdea015c86a4c9bae7e8606b76c439b209
Daniel Cooper <daniel.c321@stu.nebo.edu>
2022-11-10 12:19:00 -0700

    Stuff in Roguelike

--- RogueLike/Assets/Scripts/MoveAndShoot.cs
+++ RogueLike/Assets/Scripts/MoveAndShoot.cs
@@ -8,13 +8,16 @@ public class MoveAndShoot : MonoBehaviour
     public float stopDistance;
     public float retreatDistance;
     private  Transform target;
+    private float shotDelay;
     public float startDelay;
 
+    public GameObject projectile;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>(); 
+        shotDelay = startDelay;
     }
 
     // Update is called once per frame
@@ -25,17 +28,33 @@ public class MoveAndShoot : MonoBehaviour
        {
             transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);// Keep Moving towards player
        } 
-       
        else if(Vector2.Distance(transform.position, target.position) < stopDistance && Vector2.Distance(transform.position, target.position) > retreatDistance)
        {
            transform.position = this.transform.position;//Stay put
        }
-       
        else if(Vector2.Distance(transform.position, target.position) < retreatDistance)
        {
            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);// Running Away from the player
        }       
-       
+        //Ranged Attack Mechanic
+       if(shotDelay <= 0)
+       {
+          Attack(); 
+       }
+       else
+       {
+           shotDelay -= Time.deltaTime;
+       }
+
+    }
+    void Attack()
+    {
+       if(Vector2.Distance(transform.position, target.position) > stopDistance)
+       {           
+           transform.position = this.transform.position;//Stay put
+           Instantiate(projectile, transform.position, Quaternion.identity);//Fire Projectiles
+           shotDelay = startDelay; // Reset ShotDelay
+       } 
     }
 
         
